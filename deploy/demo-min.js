e11398258e2feef24d9cdd6f32320b5250a06ff6
laurenbulka <lab439@drexel.edu>
2020-09-27 14:20:03 -0400

    added gsap

---
+++ deploy/demo-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";var timeOfAnimation=3;gsap.to(".red-box",{duration:timeOfAnimation,borderRadius:"20"}),gsap.to(".red-box",{duration:timeOfAnimation,rotation:360,repeat:3,delay:timeOfAnimation});
