33900c02faaf1d040955bfd1b2846f1b9c44ba34
laurenbulka <lab439@drexel.edu>
2020-09-29 19:26:58 -0400

    animation timeline started

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -4,17 +4,18 @@ import {gsap} from "gsap";
 /* -----------------
     Demo Script
 ----------------- */
+var boxAnimationTL = gsap.timeline();
 
 
+boxAnimationTL.to(".red-box",{duration: 0, borderRadius: "100"})
+        .to(".red-box",{duration: 3, x:800, y:200})
+        .to(".red-box",{duration: 2, x:300, y:400})
+        .to(".red-box",{duration: 2, scale: 2}, "2");
 
-gsap.to(".red-box",{duration: 0, borderRadius: "100"});
-gsap.to(".red-box",{duration: 4, x:800, y:200, delay: 1});
-gsap.to(".red-box",{duration: 4, x:300, y:400, delay: 4});
-gsap.to(".red-box",{duration: 2, scale: 2, delay:3});
-
-gsap.to(".teal-box",{duration: 0, borderRadius: "100"});
-gsap.from(".teal-box",{duration: 2, x:400, y:300, delay:1});
-gsap.to(".teal-box",{duration: 4, x:800, y:200, delay: 4});
-gsap.from(".teal-box",{duration: 2, borderRadius: "200", delay: 5});
-gsap.from(".teal-box",{duration: 2, rotation:270, borderRadius:"20", delay:3});
-gsap.to(".teal-box",{duration: 2, scale: 2, delay:4});
+boxAnimationTL.to(".teal-box",{duration: 0, borderRadius: "20"},"-=7")
+        .from(".teal-box",{duration: 3, x:400, y:300},"-=5")
+        .to(".teal-box",{duration: 2, rotation:270, borderRadius:"20"},"-=3")
+        .to(".teal-box",{duration: 1, borderRadius: "100"},"-=3")
+        .to(".teal-box",{duration: 2, x:800, y:200},"-=2")
+        .to(".teal-box",{duration: 2, scale: 2},"-=2")
+        .to(".teal-box",{duration: 2, borderRadius: "200"},"-=2");
